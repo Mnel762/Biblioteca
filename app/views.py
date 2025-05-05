cb5f52736ddfdf008214a00a344841f9b4a92567
Emmanuel da Silva Pereira Reis <15745434660@MUZAMBINHO.EDU.BR>
2025-05-05 17:01:25 -0300

    inicio

---
+++ app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.views import View
+from django.contrib import messages
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        autores = Autor.objects.all()
+        livros = Livro.objects.all()
+        cidades = Cidade.objects.all()
+        return render(request, 'index.html', {
+            'autores': autores,
+            'livros': livros,
+            'cidades': cidades,
+        })
+    def post(self, request):
+        pass
+
+class LivrosView(View):
+    def get(self, request, *args, **kwargs):
+        livros = Livro.objects.all()
+        return render(request, 'livros.html', {'livros': livros})
+
+# def post(self, request, *args, **kwargs):
+
+class EmprestimoView(View):
+    def get(self, request, *args, **kwargs):
+        reservas = Emprestimo.objects.all()
+        return render(request, 'reserva.html', {'reservas': reservas})
+
+class CidadesView(View):
+    def get(self, request, *args, **kwargs):
+        cidades = Cidade.objects.all()
+        return render(request, 'cidade.html', {'cidades': cidades})
+
+class AutoresView(View):
+    def get(self, request, *args, **kwargs):
+        autores = Autor.objects.all()
+        return render(request, 'autor.html', {'autores': autores})
+
+class EditorasView(View):
+    def get(self, request, *args, **kwargs):
+        editoras = Editora.objects.all()
+        return render(request, 'editora.html', {'editoras': editoras})
+
+class LeitoresView(View):
+    def get(self, request, *args, **kwargs):
+        leitores = Leitor.objects.all()
+        return render(request, 'leitor.html', {'leitores': leitores})
+
+class GenerosView(View):
+    def get(self, request, *args, **kwargs):
+        generos = Genero.objects.all()
+        return render(request, 'genero.html', {'generos': generos})
+    
+class DeleteLivroView(View):
+    def get(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        livro.delete()
+        messages.success(request, 'Livro excluído com sucesso!')
+        return redirect('index')
